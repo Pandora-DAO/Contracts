b7dd355dc10efe3299808923fd99cf0a39aa502d
PandoraDAO <125491763+Pandora-DAO@users.noreply.github.com>
2023-02-16 23:20:34 +0100

    Update StakingWarmup.sol

--- StakingWarmup.sol
+++ StakingWarmup.sol
@@ -77,17 +77,17 @@ interface IERC20 {
 contract StakingWarmup {
 
     address public immutable staking;
-    address public immutable sOHM;
+    address public immutable xPDRAI;
 
-    constructor ( address _staking, address _sOHM ) {
+    constructor ( address _staking, address _xPDRAI ) {
         require( _staking != address(0) );
         staking = _staking;
-        require( _sOHM != address(0) );
-        sOHM = _sOHM;
+        require( _xPDRAI != address(0) );
+        xPDRAI = _xPDRAI;
     }
 
     function retrieve( address _staker, uint _amount ) external {
         require( msg.sender == staking );
-        IERC20( sOHM ).transfer( _staker, _amount );
+        IERC20( xPDRAI ).transfer( _staker, _amount );
     }
 }
